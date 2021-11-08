5990bb1fe3ebc386ea8011a95b35226b1f8fbe6f
stephankurpjuweit <stephan.kurpjuweit@thinking-apps.com>
2021-11-08 07:24:31 +0100

    adjusted numbering

--- lib/1_1_alignment/main.dart
+++ lib/1_1_alignment/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:layouting/1_alignment/1_center.dart';
-import 'package:layouting/1_alignment/2_fractionally_sized_box.dart';
-import 'package:layouting/1_alignment/3_align.dart';
-import 'package:layouting/1_alignment/4_padding.dart';
-import 'package:layouting/1_alignment/5_baseline.dart';
-import 'package:layouting/1_alignment/6_safe_area.dart';
+import 'package:layouting/1_1_alignment/1_center.dart';
+import 'package:layouting/1_1_alignment/2_fractionally_sized_box.dart';
+import 'package:layouting/1_1_alignment/3_align.dart';
+import 'package:layouting/1_1_alignment/4_padding.dart';
+import 'package:layouting/1_1_alignment/5_baseline.dart';
+import 'package:layouting/1_1_alignment/6_safe_area.dart';
 
 void main() {
   runApp(MaterialApp(home: Scaffold(body: NavigateWidget())));
